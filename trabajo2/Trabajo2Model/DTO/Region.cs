792d2e193ad6aed07fa359e1908206c947f3f033
Fabian Astorga <fabian.astorga02@inacapmail.cl>
2021-05-23 22:19:55 -0400

    agregado de clases faltantes

---
+++ trabajo2/Trabajo2Model/DTO/Region.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo2Model.DTO
+{
+    public class Region
+    {
+        private int nroRegion;
+        private String nombreRegion;
+
+        public string NombreRegion { get => nombreRegion; set => nombreRegion = value; }
+        public int NroRegion { get => nroRegion; set => nroRegion = value; }
+    }
+}
---
+++ trabajo2/Trabajo2Model/DTO/ValorElectricidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo2Model.DTO
+{
+    public class ValorElectricidad
+    {
+        private int idElectricidad;
+        private float tarifa;
+
+        public int IdElectricidad { get => idElectricidad; set => idElectricidad = value; }
+        public float Tarifa { get => tarifa; set => tarifa = value; }
+    }
+}
